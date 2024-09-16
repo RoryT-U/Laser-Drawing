3754e264d89b7f9dc389aff06724efab95b36a3a
jval0006 <jval0006@student.monash.edu>
2024-09-16 16:19:55 +1000

    CV and pong improvements

--- Python Code/cv/transmit_cam.py
+++ Python Code/cv/transmit_cam.py
@@ -5,11 +5,14 @@ import time
 import serial
 import pandas as pd
 from scipy.spatial import KDTree
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+
+segmentor = SelfiSegmentation()
 
 def nothing(x):
     pass
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Error: Could not open video stream.")
@@ -19,11 +22,12 @@ cv2.namedWindow('canny', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('canny', 500, 500)  # Set the desired window size (width, height)
 switch = 'OFF / ON'
 cv2.createTrackbar(switch, 'canny', 1, 1, nothing)
-cv2.createTrackbar('Lower', 'canny',70,255,nothing)
-cv2.createTrackbar('Upper', 'canny',120,255,nothing)
+cv2.createTrackbar('Lower', 'canny',255,255,nothing)
+cv2.createTrackbar('Upper', 'canny',255,255,nothing)
+cv2.createTrackbar('BG Threshold', 'canny',0,1000,nothing)
 
 serialPort = serial.Serial(
-    port="COM5", baudrate=1500000, bytesize=serial.EIGHTBITS, timeout=0, stopbits=serial.STOPBITS_ONE,parity=serial.PARITY_NONE
+    port="COM4", baudrate=1500000, bytesize=serial.EIGHTBITS, timeout=0, stopbits=serial.STOPBITS_ONE,parity=serial.PARITY_NONE
 )
 isPressed = False
 
@@ -34,6 +38,7 @@ while True:
     key = cv2.waitKey(1) & 0xFF
 
     ret, frame = cap.read()
+    frame = cv2.resize(frame, (255, 255))
 
     if not ret:
         print("Error: Failed to capture image")
@@ -43,7 +48,9 @@ while True:
     lower = cv2.getTrackbarPos('Lower','canny')
     upper = cv2.getTrackbarPos('Upper', 'canny')
     s = cv2.getTrackbarPos(switch,'canny')
+    BG_threshold = cv2.getTrackbarPos('BG Threshold', 'canny')
 
+    frame = segmentor.removeBG(frame, (0,255,0) ,cutThreshold=800/1000)
 
     # Ensure upper >= lower
     if upper < lower:
@@ -66,7 +73,7 @@ while True:
         if len(points) < 2:
             continue
 
-        keep_ratio = 3000/len(points)
+        keep_ratio = 1500/len(points)
 
 
         # Randomly select a subset of points based on the keep_ratio
@@ -95,20 +102,20 @@ while True:
         # Function to find the closest unvisited point
         def find_closest_unvisited_point(current_index):
             distances, indices = tree.query(selected_points[current_index], k=len(selected_points))
-            for idx in indices:
-                if not visited[idx]:
-                    return idx
-            return None
+            for i in range(len(indices)):
+                if not visited[indices[i]]:
+                    return indices[i], distances[i]
+            return None, 0
 
         # Start from the first point
         current_index = 0
         visited[current_index] = True
-        path.append((selected_points[current_index][0], selected_points[current_index][1]))
+        path.append((selected_points[current_index][0], selected_points[current_index][1],0))
 
         start_point = (selected_points[current_index][0], selected_points[current_index][1])
         
         for _ in range(1, num_points_to_keep):
-            next_index = find_closest_unvisited_point(current_index)
+            next_index, dist = find_closest_unvisited_point(current_index)
             if next_index is None:
                 break
             
@@ -117,13 +124,18 @@ while True:
             
             # Mark the next point as visited
             visited[next_index] = True
-            path.append((selected_points[next_index][0], selected_points[next_index][1]))
+            LED = 255
+            if dist > 10:
+                LED = 0
+                for _ in range(5):
+                    path.append((selected_points[next_index][0], selected_points[next_index][1], LED))
+            path.append((selected_points[next_index][0], selected_points[next_index][1], LED))
             
             # Move to the next point
             start_point = end_point
             current_index = next_index
 
-        path.append((selected_points[0][0], selected_points[0][1]))
+        #path.append((selected_points[0][0], selected_points[0][1], 255))
 
         # output = bytearray()
         # for i in range(len(path)):
@@ -135,8 +147,15 @@ while True:
         # serialPort.write(output)
 
         output = []
+        swap = True
         for i in range(len(path)):
-            point = [255-int(path[i][0]//1.9), 255-int(path[i][1]//2.51), 255]
+            X = 255-int(path[i][0]//1.9)
+            Y = 255-int(path[i][1]//2.51)
+            if swap:
+                X = 255-int(path[i][1])
+                Y = 255-int(path[i][0])
+
+            point = [X, Y, int(path[i][2])]
             #print(point)
             output.extend(point)
 
@@ -146,50 +165,6 @@ while True:
         print(f"IMG Sent {len(output)} bytes \n\n\n")
 
         IMG = bytearray(output)
-
-
-        def readCSV(filename):
-            with open(filename, 'r') as csvfile:
-                csvreader = csv.reader(csvfile)
-                row = next(csvreader)
-
-                if row[-1] == '':
-                    row = row[:-1]
-                int_row = [int(value) for value in row]
-
-                scale = max(int_row) / 255
-
-                scaled_row = [int(value // scale) for value in int_row]
-
-                return scaled_row
-
-        SWAP_XY = False
-        FLIP_X = True
-        FLIP_Y = True
-
-        def getBytesOfCSV(filename):
-            xLst = readCSV(f"Python Code\\Coordinate\\converted_csvs\\{filename}-{'y' if SWAP_XY else 'x'}.csv")
-            yLst = readCSV(f"Python Code\\Coordinate\\converted_csvs\\{filename}-{'x' if SWAP_XY else 'y'}.csv")
-
-            xLst = [255-val for val in xLst] if FLIP_X else xLst
-            yLst = [255-val for val in yLst] if FLIP_Y else yLst
-
-            output = []
-            for i in range(len(xLst)):
-                output.extend([xLst[i], yLst[i], 255])
-
-            return bytearray(output + [13,13,13])
-
-
-            #### READ DATA
-        output = getBytesOfCSV("mike-300")
-        #serialPort.write((output))
-        # print(output)
-        # MIKE = output
-        # print(f"MIKE sent {len(output)} bytes")
-
-        #time.sleep(0.1)
-
 
     if key == 32: # press space to capture image
         coords = np.column_stack(np.where(edges > 0))
